17847d667b61b786e427cbcd32fbcbcad42e1a35
Joan Domingo <joands@gmail.com>
2021-08-30 17:01:01 +0200

    Add a loading page before the DOM is loaded

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,50 +1,46 @@
 import { MenuEntry } from '@duhd4h/global-uikit'
-import { BASE_MAIN_FRONTEND_URL_FARMS, BASE_MAIN_FRONTEND_URL_HOME, BASE_MAIN_FRONTEND_URL_POOLS } from '../../config'
+import {
+  BASE_MAIN_FRONTEND_URL_CONTACT,
+  BASE_MAIN_FRONTEND_URL_FARMS,
+  BASE_MAIN_FRONTEND_URL_HOME,
+  BASE_MAIN_FRONTEND_URL_OPTIMIZER,
+  BASE_MAIN_FRONTEND_URL_POOLS,
+  BASE_MAIN_FRONTEND_URL_VAULTS
+} from '../../config'
 
 const config: MenuEntry[] = [
   {
     label: 'Home',
-    icon: 'HomeIcon',
     href: BASE_MAIN_FRONTEND_URL_HOME,
   },
   {
     label: 'Exchange',
-    icon: 'PoolIcon',
     href: '/swap',
   },
   {
     label: 'Liquidity',
-    icon: 'PoolIcon',
     href: '/pool',
   },
   {
     label: 'Farms',
-    icon: 'FarmIcon',
     href: BASE_MAIN_FRONTEND_URL_FARMS,
   },
   {
     label: 'Pools',
-    icon: 'PoolIcon',
     href: BASE_MAIN_FRONTEND_URL_POOLS,
   },
   {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Contact',
-        href: '',
-      },
-      {
-        label: 'Voting',
-        href: '',
-      },
-      {
-        label: 'Github',
-        href: 'https://github.com/Global-Repo',
-      },
-    ],
+    label: 'Vaults Globals',
+    href: BASE_MAIN_FRONTEND_URL_VAULTS,
   },
+  {
+    label: 'Optimizer',
+    href: BASE_MAIN_FRONTEND_URL_OPTIMIZER,
+  },
+  {
+    label: 'Contact',
+    href: BASE_MAIN_FRONTEND_URL_CONTACT,
+  }
 ]
 
 export default config
