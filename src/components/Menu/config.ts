9f440fea5d2359e10ad3ed8887f5d3e8c83c13a7
Joan Domingo <joands@gmail.com>
2021-09-08 21:56:19 +0200

    Update menu routes

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -1,10 +1,9 @@
 import { MenuEntry } from '@duhd4h/global-uikit'
 import {
-  BASE_MAIN_FRONTEND_URL_CONTACT,
   BASE_MAIN_FRONTEND_URL_FARMS,
   BASE_MAIN_FRONTEND_URL_HOME,
-  BASE_MAIN_FRONTEND_URL_OPTIMIZER,
-  BASE_MAIN_FRONTEND_URL_POOLS,
+  BASE_MAIN_FRONTEND_URL_POOLS_GLOBAL,
+  BASE_MAIN_FRONTEND_URL_POOLS_TOKEN,
   BASE_MAIN_FRONTEND_URL_VAULTS
 } from '../../config'
 
@@ -26,20 +25,20 @@ const config: MenuEntry[] = [
     href: BASE_MAIN_FRONTEND_URL_FARMS,
   },
   {
-    label: 'Pools',
-    href: BASE_MAIN_FRONTEND_URL_POOLS,
+    label: 'Stake Global',
+    href: BASE_MAIN_FRONTEND_URL_POOLS_GLOBAL,
   },
   {
-    label: 'Vaults Globals',
+    label: 'Stake Token',
+    href: BASE_MAIN_FRONTEND_URL_POOLS_TOKEN,
+  },
+  {
+    label: 'Vaults',
     href: BASE_MAIN_FRONTEND_URL_VAULTS,
   },
   {
-    label: 'Optimizer',
-    href: BASE_MAIN_FRONTEND_URL_OPTIMIZER,
-  },
-  {
-    label: 'Contact',
-    href: BASE_MAIN_FRONTEND_URL_CONTACT,
+    label: 'BeGlobal',
+    href: 'https://beglobal.finance/',
   }
 ]
 
