f8eafca6e09c90a92f602bb8a46603e3561d9804
Joan Domingo <joands@gmail.com>
2021-09-13 18:41:07 +0200

    update visual styles

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    background: linear-gradient(45deg, #1748a0, #1c102b);
     background-color: ${({ theme }) => theme.colors.background};
 
     img {
